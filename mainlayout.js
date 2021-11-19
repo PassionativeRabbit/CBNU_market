63ae38aeaf3dc5e331a8a3ac2c10ca480d64da59
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2021-11-19 11:17:34 +0900

    초기

---
+++ mainlayout.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {StyleSheet, View, Text, LayoutAnimation, TouchableOpacity, } from 'react-native';
+import {Dimensions,} from "react-native"
+import {
+    NativeBaseProvider,
+    Box,
+    HStack,
+    VStack,
+    Image,
+} from 'native-base';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export default function mainlayout({ navigation }) {
+    const pressHandler = () => {
+        navigation.navigate('login');
+    }
+
+    return (
+            <NativeBaseProvider>
+                <Box>
+                    <Box style={{ backgroundColor : 'yellow', height : '40%' }}>
+                        <HStack>
+                            <Icon name="bars" size={30} color="#333" />
+                            <Icon name="search" size={30} color="#333" style={{marginLeft : '80%'}}></Icon>
+                            <Icon name="bell" size={30} color="#333"></Icon>
+                        </HStack>
+                   </Box>
+
+                    <Box>
+                        <View>
+                        
+                        </View>
+                    </Box>
+                </Box>
+            </NativeBaseProvider>
+    )
+
+}
