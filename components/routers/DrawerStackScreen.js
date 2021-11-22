61fba495cff54dca4bcc4ae8b6e6f2705a04d155
Su eun <65611954+beeee2@users.noreply.github.com>
2021-11-22 15:27:31 +0900

    Update DrawerStackScreen.js

--- components/routers/DrawerStackScreen.js
+++ components/routers/DrawerStackScreen.js
@@ -3,8 +3,65 @@ import { createDrawerNavigator, DrawerContentScrollView, DrawerItem } from '@rea
 import { createStackNavigator } from '@react-navigation/stack';
 import TabStackScreen from './TabStackScreen';
 import MenuButton from './MenuButton';
+import { Ionicons } from '@expo/vector-icons';
 
 const MainDrawer = createDrawerNavigator();
+
+const CustomDrawer = ({navigation}) => {
+    const goToStack = (stackName) => {
+        navigation.navigate(stackName);
+    };
+
+    return (
+        <DrawerContentScrollView>
+            <DrawerItem
+                label="HOME"
+                onPress={() => goToStack('homestack')}
+                style={{
+                    borderBottomWidth:1,
+                    borderRadius:0,
+                    borderColor:'#ecf0f1'
+                }}
+            >
+            </DrawerItem>
+            <DrawerItem
+                label="Search"
+                onPress={() => goToStack('searchstack')}
+                style={{
+                    borderBottomWidth:1,
+                    borderRadius:0,
+                    borderColor:'#ecf0f1'
+                }}
+            >
+            </DrawerItem>
+            <DrawerItem
+                label="Shopping"
+                onPress={() => goToStack('shopstack')}
+                style={{
+                    borderBottomWidth:1,
+                    borderRadius:0,
+                    borderColor:'#ecf0f1'
+                }}
+            >
+            </DrawerItem>
+            <DrawerItem
+                label="Chatting"
+                onPress={() => goToStack('chatliststack')}
+                style={{
+                    borderBottomWidth:1,
+                    borderRadius:0,
+                    borderColor:'#ecf0f1'
+                }}
+            >
+            </DrawerItem>
+            <DrawerItem
+                label="My Page"
+                onPress={() => goToStack('mypagestack')}
+            >
+            </DrawerItem>
+        </DrawerContentScrollView>
+    );
+};
 
 const MainDrawerStackScreen = () => {
     return (
@@ -12,11 +69,12 @@ const MainDrawerStackScreen = () => {
             screenOptions={{
                 headerShown: false
             }}
+            drawerContent={ ({navigation}) => <CustomDrawer navigation={navigation} />}
         >
             <MainDrawer.Screen
                 name="tabstack"
                 component={TabStackScreen}
-                optins={{}}
+                options={{}}
             >
             </MainDrawer.Screen>
         </MainDrawer.Navigator>
